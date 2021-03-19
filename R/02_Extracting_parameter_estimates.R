fd32e81df5275c566ff38a456430bbb1792dc889
Lauren <76951039+lauren-smith-r@users.noreply.github.com>
2021-03-19 13:06:35 +0000

    added comments to code

--- R/02_Extracting_parameter_estimates.R
+++ R/02_Extracting_parameter_estimates.R
@@ -2,9 +2,9 @@ require(ggplot2)
 require(ggpubr)
 setwd("")
 
-area_dmat_italy <- read.csv("italy_Distances.csv")[,-1]
-area_dmat_ukr <- read.csv("Ukraine_Distances.csv")[,-1]
-time_dmat_italy <- time_dmat_italy <- matrix(c(
+area_dmat_italy <- read.csv("italy_Distances.csv")[,-1] # load distances between study sites for study sites in Italy
+area_dmat_ukr <- read.csv("Ukraine_Distances.csv")[,-1] # load distances between study sites for study sites in Ukraine
+time_dmat_italy <- time_dmat_italy <- matrix(c( # create a matrix of time between primary periods for Italy
   # P=1,
   0, 3, 6, 12, 15,
   # P=2,
@@ -19,7 +19,7 @@ time_dmat_italy <- time_dmat_italy <- matrix(c(
 nrow = 5, ncol=5, byrow = TRUE
 )
 
-time_dmat_ukr <- matrix(c(
+time_dmat_ukr <- matrix(c( # create a matrix of time between primary periods for Ukraine
   # P=1,
   0, 3, 6, 12, 15,
   # P=2,
@@ -34,12 +34,12 @@ time_dmat_ukr <- matrix(c(
 nrow = 5, ncol=5, byrow = TRUE
 )
 
-logit <- function(x) log(x / (1-x))
+logit <- function(x) log(x / (1-x)) # function for determining results on logit scale
 
-inv_logit <- function(x) exp(x) / (1 + exp(x))
-HDI <- function(x) coda::HPDinterval(coda::as.mcmc(x), prob=0.95) 
+inv_logit <- function(x) exp(x) / (1 + exp(x)) # function for taking the inverse logit (e.g. to determine results on probability scale)
+HDI <- function(x) coda::HPDinterval(coda::as.mcmc(x), prob=0.95)  # function for extracting the highest density intervals
 
-ps_simple <- read.csv("lauren-mcmc-samples-simple-19-12-2019.csv")
+ps_simple <- read.csv("lauren-mcmc-samples-simple-19-12-2019.csv") # read in model results
 
 ###Extracting parameters####
 
@@ -53,52 +53,48 @@ HDI(inv_logit(ps_simple$mu_lodd_delta_italy))
 inv_logit(-1.10)
 
 #Superpopulation
-mean(ps_simple$superpop_italy.4)
-HDI(ps_simple$superpop_italy.4)
+mean(ps_simple$superpop_italy.1) # estimate the mean of the superpopulation for _italy or _ukraine for study sites 1-4 (.1 to .4)
+HDI(ps_simple$superpop_italy.1)
 
 #N_hat
-mean(ps_simple$N_hat_italy.5.4)
-HDI(ps_simple$N_hat_italy.5.4)
+mean(ps_simple$N_hat_italy.1.1) # estimate the mean of the population size (N_hat) for primary periods 1 to 5 and study sites 1 to 4 (.primary_period.study_site)
+HDI(ps_simple$N_hat_italy.1.1)
 
-# Between country comparisons
-mean( ps_simple$mu_lodd_phi_italy - ps_simple$mu_lodd_phi_ukr )
+# Between country comparisons for detection probability (delta), survival (phi) and recruitment (psi)
+mean( ps_simple$mu_lodd_phi_italy - ps_simple$mu_lodd_phi_italy ) # on log odds scale
 HDI( ps_simple$mu_lodd_phi_italy - ps_simple$mu_lodd_phi_ukr )
-mean( (inv_logit(ps_simple$mu_lodd_phi_italy)) - (inv_logit(ps_simple$mu_lodd_phi_ukr)))
+mean( (inv_logit(ps_simple$mu_lodd_phi_italy)) - (inv_logit(ps_simple$mu_lodd_phi_ukr))) # on probability scale
 HDI( (inv_logit(ps_simple$mu_lodd_phi_italy)) - (inv_logit(ps_simple$mu_lodd_phi_ukr)))
-mean(exp ( (ps_simple$mu_lodd_delta_italy) - ((ps_simple$mu_lodd_delta_ukr))))
+mean(exp ( (ps_simple$mu_lodd_delta_italy) - ((ps_simple$mu_lodd_delta_ukr)))) # as odds
 HDI(exp ( (ps_simple$mu_lodd_delta_italy) - ((ps_simple$mu_lodd_delta_ukr))))
 
 hist((ps_simple$mu_lodd_delta_italy) - (ps_simple$mu_lodd_delta_ukr))
 
 #Sigma dogs [study site, 1recruitment/2survival/3detection]
-mean( ps_simple$sigma_dog_ukr.4.3 )
+mean( ps_simple$sigma_dog_ukr.4.3 ) # random effect of dog per study site on recruitment/survival/detection parameters
 HDI( ps_simple$sigma_dog_ukr.4.3 )
 
 ###Explanatory variables ####
-mean(ps_simple$sigma_dog_ukr.4.3)
-HDI(ps_simple$sigma_dog_ukr.4.3)
-# mean(inv_logit(ps_simple$sigma_dog_italy.1.2))
 
-###log odds of beta_weekend effect on delta (anchored on weekend)####
+###log odds of weekend effect (beta_weekend) on detection probability (delta) (anchored on weekend) for Italy (_italy) and Ukraine (_ukr) ####
 mean(ps_simple$beta_weekend_ukr)
 HDI(ps_simple$beta_weekend_ukr)
 
-#Converting to odds
+#Converting the weekend effect on detection to the odds scale
 mean(exp(ps_simple$beta_weekend_italy))
 HDI(exp(ps_simple$beta_weekend_italy))
-
-#Probability beta_weekend effect
+#Converting the weekend effect on detection to the probability scale
 mean(inv_logit(ps_simple$beta_weekend_italy))
 HDI(inv_logit(ps_simple$beta_weekend_italy))
-#Weekday probability
+#Calculation probability of detecting a dog on the weekday
 mean(inv_logit(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_weekend_ukr))
 HDI(inv_logit(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_weekend_ukr))
-#Odds of weekday detection
-mean(exp(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_weekend_ukr))
-HDI(exp(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_weekend_ukr))
 #Weekend probability
 mean(inv_logit(ps_simple$mu_lodd_delta_ukr))
 HDI(inv_logit(ps_simple$mu_lodd_delta_ukr))
+#Calcuating the odds of detecting a dog on the weekday
+mean(exp(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_weekend_ukr))
+HDI(exp(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_weekend_ukr))
 #Odds of weekend detection
 mean(exp(ps_simple$mu_lodd_delta_ukr))
 HDI(exp(ps_simple$mu_lodd_delta_ukr))
@@ -110,18 +106,12 @@ mean(exp ((ps_simple$mu_lodd_delta_ukr + ps_simple$beta_weekend_ukr) - (ps_simpl
 HDI(exp ((ps_simple$mu_lodd_delta_ukr + ps_simple$beta_weekend_ukr) - (ps_simple$mu_lodd_delta_ukr)))
 
 
-###log odds of beta_market effect on delta (anchored on market day)####
-# Anchored on non-market day?
-
+###log odds of beta_market effect on detection (delta) (anchored on non-market day) for Italy (_italy) and Ukraine (_ukr) ####
 mean(ps_simple$beta_market_ukr)
 HDI(ps_simple$beta_market_ukr)
 #Converting to odds ratio
 mean(exp(ps_simple$beta_market_italy))
 HDI(exp(ps_simple$beta_market_italy))
-# #Probability beta_market effect
-# mean(inv_logit(ps_simple$beta_market_ukr))
-# HDI(inv_logit(ps_simple$beta_market_ukr))
-
 #Marketday probability
 mean(inv_logit(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_market_ukr))
 HDI(inv_logit(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_market_ukr))
@@ -141,13 +131,13 @@ HDI( (inv_logit(ps_simple$mu_lodd_delta_ukr + ps_simple$beta_market_ukr)) - inv_
 mean(exp ((ps_simple$mu_lodd_delta_ukr + ps_simple$beta_market_ukr) - (ps_simple$mu_lodd_delta_ukr)))
 HDI(exp ((ps_simple$mu_lodd_delta_ukr + ps_simple$beta_market_ukr) - (ps_simple$mu_lodd_delta_ukr)))
 
-###log odds of beta_rain effect on delta (anchored on market day)####
+###log odds of rain (beta_rain) effect on detection (delta) for Italy (_italy) and Ukraine (_ukr) ####
 mean(ps_simple$beta_rain_italy)
 HDI(ps_simple$beta_rain_italy)
 #converting to odds ratios
 mean(exp(ps_simple$beta_rain_ukr))
 HDI(exp(ps_simple$beta_rain_ukr))
-#Probability beta_market effect
+#Probability beta_rain effect
 mean(inv_logit(ps_simple$beta_rain_italy))
 HDI(inv_logit(ps_simple$beta_rain_italy))
 #Difference in means
@@ -155,15 +145,14 @@ mean( (ps_simple$mu_lodd_delta_italy + ps_simple$beta_rain_italy) - ps_simple$mu
 mean( (inv_logit(ps_simple$mu_lodd_delta_italy + ps_simple$beta_rain_italy)) - inv_logit(ps_simple$mu_lodd_delta_italy))
 HDI( (inv_logit(ps_simple$mu_lodd_delta_italy + ps_simple$beta_rain_italy)) - inv_logit(ps_simple$mu_lodd_delta_italy))
 
-inv_logit(-1.10)
 
-###log odds of beta_temp effect on delta (anchored on market day)####
+###log odds of temperature (beta_temp) effect on detection (delta) for Italy (_italy) and Ukraine (_ukr) ####
 mean(ps_simple$beta_temp_italy)
 HDI(ps_simple$beta_temp_italy)
 #Converting to odds ratios
 mean(exp(ps_simple$beta_temp_ukr))
 HDI(exp(ps_simple$beta_temp_ukr))
-#Probability beta_market effect
+#Probability beta_temp effect
 mean(inv_logit(ps_simple$beta_temp_italy))
 HDI(inv_logit(ps_simple$beta_temp_italy))
 #Difference in means
@@ -172,11 +161,11 @@ mean( (inv_logit(ps_simple$mu_lodd_delta_italy + ps_simple$beta_temp_italy)) - i
 HDI( (inv_logit(ps_simple$mu_lodd_delta_italy + ps_simple$beta_temp_italy)) - inv_logit(ps_simple$mu_lodd_delta_italy))
 
 
-###log odds of sex effect on survival (female -> male)####
+###log odds of sex effect on survival (.1 phi) (female -> male) for Italy (_italy) and Ukraine (_ukr) ####
 mean(ps_simple$beta_sex_ukr.1)
 HDI(ps_simple$beta_sex_ukr.1)
-
-mean(inv_logit(ps_simple$mu_lodd_phi_italy + ps_simple$sigma_dog_italy.1.2))
+# effect on probability scale
+mean(inv_logit(ps_simple$mu_lodd_phi_italy + ps_simple$sigma_dog_italy.1.2)) # sigma_dog[study site, 1recruitment/2survival/3detection]
 sd(inv_logit(ps_simple$mu_lodd_phi_italy + ps_simple$sigma_dog_italy.1.2))
 
 #Converting to odds ratios
@@ -231,46 +220,46 @@ HDI(
 
 
 
-###log odds of sex effect on delta (female -> male)####
+###log odds of sex effect on detection (.2 delta) (female -> male)####
 mean(ps_simple$beta_sex_italy.2)
 HDI(ps_simple$beta_sex_italy.2)
 #Converting to odds ratios
 mean(exp(ps_simple$beta_sex_ukr.2))
 HDI(exp(ps_simple$beta_sex_ukr.2))
 
-#probabilty of sex effect on delta
+#probabilty of sex effect on detection (delta)
 mean(inv_logit(ps_simple$beta_sex_italy.2))
 HDI(inv_logit(ps_simple$beta_sex_italy.2))
 
-# total log odds of females' delta 
-mean(ps_simple$mu_lodd_phi_italy)
-HDI(ps_simple$mu_lodd_phi_italy)
+# total log odds of females' detection (delta)
+mean(ps_simple$mu_lodd_delta_italy)
+HDI(ps_simple$mu_lodd_delta_italy)
 
-#Odds of female delta
-mean(exp(ps_simple$mu_lodd_phi_italy))
-HDI(exp(ps_simple$mu_lodd_phi_italy))
+#Odds of female detection (delta)
+mean(exp(ps_simple$mu_lodd_delta_italy))
+HDI(exp(ps_simple$mu_lodd_delta_italy))
 
 # probability of female delta
-mean(inv_logit(ps_simple$mu_lodd_phi_italy))
-HDI(inv_logit(ps_simple$mu_lodd_phi_italy))
+mean(inv_logit(ps_simple$mu_lodd_delta_italy))
+HDI(inv_logit(ps_simple$mu_lodd_delta_italy))
 
 # total log odds of males' delta
-mean(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2)
-HDI(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2)
+mean(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2)
+HDI(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2)
 # Odds
-mean(exp(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2))
-HDI(exp(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2))
+mean(exp(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2))
+HDI(exp(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2))
 # probability of male delta
-mean(inv_logit(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2))
-HDI(inv_logit(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2))
+mean(inv_logit(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2))
+HDI(inv_logit(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2))
 
 # difference between males and females delta
-mean(ps_simple$mu_lodd_phi_italy) - mean(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2)
+mean(ps_simple$mu_lodd_delta_italy) - mean(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2)
 mean(
-  inv_logit(ps_simple$mu_lodd_phi_italy) - inv_logit(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2)
+  inv_logit(ps_simple$mu_lodd_delta_italy) - inv_logit(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2)
 ) 
 HDI(
-  inv_logit(ps_simple$mu_lodd_phi_italy) - inv_logit(ps_simple$mu_lodd_phi_italy + ps_simple$beta_sex_italy.2)
+  inv_logit(ps_simple$mu_lodd_delta_italy) - inv_logit(ps_simple$mu_lodd_delta_italy + ps_simple$beta_sex_italy.2)
 ) 
 
 ###Estimates for primary periods####
